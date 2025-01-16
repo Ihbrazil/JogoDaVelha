71b34d1e227a6093762688d88cecdf6f90565833
ihbrazil <ihbrazil@users.noreply.github.com>
2025-01-16 17:44:24 -0300

    Atualização na identação componente Casacasa

--- componentes/Casa/index.js
+++ componentes/Casa/index.js
@@ -2,8 +2,9 @@ import { Text, View } from 'react-native';
 
 import styles from './styles';
 
-export default function Casa({jogador = null,temBordaDireita = false, temBordaInferior = false}) {
-    
+export default function Casa({jogador = null,
+                              temBordaDireita = false, temBordaInferior = false})
+{
     const larguraBordaDireita = (temBordaDireita) ? 3 : 0;
     const larguraBordaInferior = (temBordaInferior) ? 3 : 0;
 
@@ -13,9 +14,9 @@ export default function Casa({jogador = null,temBordaDireita = false, temBordaIn
             borderBottomWidth: larguraBordaInferior
         }]}>
 
-        <Text style={styles.marcadorJogador}>
-            {jogador}
-        </Text>
+            <Text style={styles.marcadorJogador}>
+                {jogador}
+            </Text>
         
         </View>
     )
